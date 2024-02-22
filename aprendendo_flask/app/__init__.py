c708f9f40714ca990127678322ea888834fb8b26
Davi Lucas <davibezerra123457@gmail.com>
2024-02-22 16:08:14 -0300

    Delete aprendendo_flask directory
    deu tudo errado

--- aprendendo_flask/app/__init__.py
+++
@@ -1,5 +0,0 @@
-from flask import Flask
-
-app = Flask(__name__) #especifica o valor do arquivo na execução, como esse é o arquivo principal, ele recebe o nome de main.
-
-from app.controllers import default
--- aprendendo_flask/app/controllers/default.py
+++
@@ -1,6 +0,0 @@
-from .. import app
-
-
-@app.route('/') #aplica a função route em cima da função index. 
-def index():
-    return 'hello world'
