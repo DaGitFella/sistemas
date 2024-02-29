4709c6ecb57a1ca13f4cecc38b9006469bab2803
Davi Lucas <davibezerra123457@gmail.com>
2024-02-29 15:09:29 -0300

    flask sessions

--- aprendendo flask/app/__init__.py
+++ aprendendo flask/app/__init__.py
@@ -1,5 +1,8 @@
 from flask import Flask
+from datetime import timedelta
 
 app = Flask(__name__)
+app.secret_key = "123"
+app.permanent_session_lifetime = timedelta(minutes=5) #defines how much time the data will be stored in the session
 
 from app.controllers import Default
--- aprendendo flask/app/controllers/Default.py
+++ aprendendo flask/app/controllers/Default.py
@@ -1,5 +1,31 @@
 from app import app
+from flask import redirect, url_for, render_template, request, session
 
 @app.route('/')
 def index():
-    return 'hello world'
+    return render_template("index.html")
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    if request.method == "POST":
+        session.permanent = True
+        user = request.form["nm"]
+        session["user"] = user #stores data on a session dictionary
+        return redirect(url_for("user"))
+    else:
+        if "user" in session:
+            return redirect(url_for("user"))
+        return render_template("login.html")
+
+@app.route('/user')
+def user():
+    if "user" in session:
+        user = session["user"]
+        return render_template('user.html', usr=user)
+    else:
+        return redirect(url_for("login"))
+
+@app.route("/logout")
+def logout():
+    session.pop("user", None)
+    return redirect(url_for("login"))
