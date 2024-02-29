8a12ef8c67e5f8779aa28388e0c8eabf90d0e183
Davi Lucas <davibezerra123457@gmail.com>
2024-02-29 16:15:44 -0300

    message flashing

--- aprendendo flask/app/controllers/Default.py
+++ aprendendo flask/app/controllers/Default.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import redirect, url_for, render_template, request, session
+from flask import redirect, url_for, render_template, request, session, flash
 
 @app.route('/')
 def index():
@@ -11,10 +11,14 @@ def login():
         session.permanent = True
         user = request.form["nm"]
         session["user"] = user #stores data on a session dictionary
+        flash("Login Succesful!")
+        flash(f"Welcome, {user}")
         return redirect(url_for("user"))
     else:
         if "user" in session:
+            flash("Already Logged In!")
             return redirect(url_for("user"))
+        
         return render_template("login.html")
 
 @app.route('/user')
@@ -23,9 +27,13 @@ def user():
         user = session["user"]
         return render_template('user.html', usr=user)
     else:
+        flash("You're now logged in!")
         return redirect(url_for("login"))
 
 @app.route("/logout")
 def logout():
+    if "user" in session: #checks if user in stored in the session dictionary keys
+        user = session["user"]
+        flash(f"{user} has logged out sucessfuly!", "info")
     session.pop("user", None)
     return redirect(url_for("login"))
