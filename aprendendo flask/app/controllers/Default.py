231d6b47d523f9cb87b31fa13da38edcf31eb6cf
Davi Lucas <davibezerra123457@gmail.com>
2024-03-05 13:44:05 -0300

    inicio dos estudos de banco de dados com flask

--- aprendendo flask/app/controllers/Default.py
+++ aprendendo flask/app/controllers/Default.py
@@ -7,24 +7,28 @@ def index():
 
 @app.route("/login", methods=["POST", "GET"])
 def login():
+    email = None
     if request.method == "POST":
         session.permanent = True
         user = request.form["nm"]
+        email = request.form["mail"]
+        session["email"] = email
         session["user"] = user #stores data on a session dictionary
         flash("Login Succesful!")
-        flash(f"Welcome, {user}")
+        flash(f"Welcome, {user}!")
         return redirect(url_for("user"))
     else:
         if "user" in session:
             flash("Already Logged In!")
             return redirect(url_for("user"))
-        
+        if "email" in session:
+            email = session["email"]
         return render_template("login.html")
 
-@app.route('/user')
+@app.route('/user,', methods=["POST", "GET"])
 def user():
     if "user" in session:
-        user = session["user"]
+        user = session["user"]       
         return render_template('user.html', usr=user)
     else:
         flash("You're now logged in!")
@@ -32,8 +36,9 @@ def user():
 
 @app.route("/logout")
 def logout():
-    if "user" in session: #checks if user in stored in the session dictionary keys
+    if "user" in session: #checks if user is stored in the session dictionary keys
         user = session["user"]
         flash(f"{user} has logged out sucessfuly!", "info")
-    session.pop("user", None)
+    session.pop("user", None) #removes the user key from the session dictionary
+    session.pop("email", None)
     return redirect(url_for("login"))
